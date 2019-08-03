7f9bcccfccd2f4b1cba890afe9eb2fe8604fecd1
vootlavenky63 <vootla.venky63@gmail.com>
2019-08-03 12:43:23 +0530

    line-line h1 tag

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react'
 
-function App() {
-  const [data, setData] = useState({ hits: [] });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        'http://hn.algolia.com/api/v1/search?query=redux',
-      );
-
-      setData(result.data);
-    };
-
-    fetchData();
-  }, []);
-
+export default function App() {
   return (
-    <ul>
-      {data.hits.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
-  );
+    <div>
+     <h1 style={{color:"red",marginLeft:"30px",paddingTop:"10px"}}></h1>
+    </div>
+  )
 }
-
-export default App;
